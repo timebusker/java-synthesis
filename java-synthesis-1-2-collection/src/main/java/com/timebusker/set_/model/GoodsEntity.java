3528e68117365b13b7d0343eca173aae430a03a4
timebusker <timebusker@vip.qq.com>
2019-03-03 23:29:29 +0800

    2019/03/03 23:29:28 ____ :sun_with_face::sunflower::palm_tree::house_with_garden::office::octocat::guitar::meat_on_bone:

--- java-synthesis-1-2-collection/src/main/java/com/timebusker/set_/model/GoodsEntity.java
+++ java-synthesis-1-2-collection/src/main/java/com/timebusker/set_/model/GoodsEntity.java
@@ -1,5 +1,6 @@
 package com.timebusker.set_.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -44,5 +45,36 @@ public class GoodsEntity {
 
     public void setPrice(int price) {
         this.price = price;
+    }
+
+
+    public GoodsEntity() {
+        super();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        new HashSet<>();
+        return super.toString();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
     }
 }
