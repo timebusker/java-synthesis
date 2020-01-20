21272d790570b40c972eb5ee6d056124d470a26d
timebusker <timebusker@qq.com>
2020-01-20 16:16:18 +0800

    2020/01/20 16:16:18 ____ :sun_with_face::sunflower::palm_tree::house_with_garden::office::octocat::guitar::meat_on_bone:

---
+++ java-synthesis-base/src/main/java/com/timebusker/generics/domain/User.java
@@ -0,0 +1,28 @@
+package com.timebusker.generics.domain;
+
+/**
+ * @DESC:泛型类测试
+ * @author:timebusker
+ * @date:2018/5/14
+ */
+public class User {
+
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
