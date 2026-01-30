b78fd5aef3741abd6624076a132732d78549424a
timebusker <timebusker@gmail.com>
2026-01-30 18:42:38 +0800

    周五 2026/01 18:42:38 _ 提交代码

--- _git_push.cmd
+++ _git_push.cmd
@@ -1,5 +1,8 @@
 color 0a
 @echo off & setlocal
+chcp 65001 > nul
+:: 后续代码使用UTF-8编码
+
 :: cd img/top-photo/
 :: call 0-rename-jpg.bat
 :: cd ..
@@ -8,5 +11,5 @@ set var=":sun_with_face::sunflower::palm_tree::house_with_garden::office::octoca
 set d=%date:~0,10%
 set t=%time:~0,8%
 git add .
-git commit -am"%d% %t% ____ %var%"
+git commit -am"%d% %t% _ 提交代码"
 git push  origin master
